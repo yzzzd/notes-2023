1e3632c2753921f70751639249b028fd4c2cdcc3
Zidd <nuvyazid@gmail.com>
2023-11-17 14:45:13 +0700

    rename main to splash

--- app/src/main/java/com/yazid/notes/SplashActivity.kt
+++ app/src/main/java/com/yazid/notes/SplashActivity.kt
@@ -3,9 +3,9 @@ package com.yazid.notes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
